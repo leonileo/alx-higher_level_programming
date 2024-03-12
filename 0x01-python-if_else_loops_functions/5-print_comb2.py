eb1758a84381a2d47d84c9440df3b9205c28b9f5
kaleb <kalebwendwessen@gmail.com>
2024-03-12 05:13:45 -0700

    Print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if (i < 10):
-        print("0{0}, ".format(i), end="")
+    if (i != 99):
+        print("{:02d}, ".format(i), end="")
     else:
-        print("{0}, ".format(i), end="" if i != 99 else "{0} \n".format(i))
+        print("{0}".format(i))
