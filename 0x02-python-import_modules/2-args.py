8bb8cad0cf6cbc9b90542742f02c7ac4d03982d8
kaleb <kalebwendwessen@gmail.com>
2024-03-14 04:09:16 -0700

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,17 @@ import sys
 
 
 def main():
-    num = len(sys.argv)
-    print("{0} arguments".format(num))
-    for i, ar in enumerate(sys.argv[1:], 1):
+    length = 0
+    num = len(sys.argv) - 1
+    if num != 1:
+        print("{0} arguments".format(num))
+    for i in sys.argv[1:]:
+        length += 1
         if num == 1:
             print("1 argument")
-            print("{0}: {1}".format(i, ar))
+            print("{0}: {1}".format(length, i))
         else:
-            print("{0}: {1}".format(i, ar))
+            print("{0}: {1}".format(length, i))
 
 
 if __name__ == "__main__":
