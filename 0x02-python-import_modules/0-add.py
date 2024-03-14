b9fa153ae23e4b054c0581bda86060adde27a53b
kaleb <kalebwendwessen@gmail.com>
2024-03-14 03:51:29 -0700

    Add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,12 @@ from add_0 import add
 a = 1
 b = 2
 c = add(a, b)
+
+
 def main():
+    """ main function """
     print("{0} + {1} = {2}".format(a, b, c))
+
+
 if __name__ == "__main__":
     main()
