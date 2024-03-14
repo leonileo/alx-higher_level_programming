50dd5d2c0e3dde2d397deea53c123bdb974477e1
kaleb <kalebwendwessen@gmail.com>
2024-03-14 04:36:37 -0700

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,16 @@ import sys
 
 
 def main():
-    num = len(sys.argv) - 1
-    if num != 1:
-        print("{0} arguments:".format(num))
-    for i in sys.argv[1:]:
-        length = sys.argv.index(i)
-        if num == 1:
-            print("1 argument:")
-            print("{0}: {1}".format(length, i))
-        else:
+    if len(sys.argv[1:]) == 1:
+        first = sys.argv[1]
+        print("1 argument:")
+        print("{0}: {1}".format(sys.argv.index(first), first))
+    elif not sys.argv[1:]:
+        print("{0} arguments.".format(len(sys.argv[1:]) ))
+    else:
+        print("{0} arguments:".format(len(sys.argv[1:])))
+        for i in sys.argv[1:]:
+            length = sys.argv.index(i)
             print("{0}: {1}".format(length, i))
 
 
