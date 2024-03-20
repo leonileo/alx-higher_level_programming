d6629dbe311981e5232e51cb4bd7e6eb6fcbcdac
kaleb <kalebwendwessen@gmail.com>
2024-03-20 06:03:12 -0700

    Print list integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,8 @@
 
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{0}".format(i))
+        print("{0}".format(int(i)))
 
 
-if __name__ == "__print_list_integer__":
+if __name__ == "__main__":
     print_list_integer()
