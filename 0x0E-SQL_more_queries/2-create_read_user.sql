542b919ae1a5d0e4e6812a088d7f76bc22c460b3
kaleb <kalebwendwessen@gmail.com>
2024-05-22 13:49:41 -0700

    Sql more queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+-- CREATE DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- CREATE USER
+CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- GRANT USER privileges
+GRANT SELECT
+ON hbtn_0d_2.*
+TO user_0d_2@localhost;
