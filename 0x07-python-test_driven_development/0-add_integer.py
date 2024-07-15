87c9a575c8014b08d392a4a51d1a758abebe1e97
leonileo <kalebwendwessen@gmail.com>
2024-07-15 04:37:10 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     """
         Return the summation of two argument
     """
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    elif not isinstance(b, (int, float)):
+    elif type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
     else:
         if isinstance(a, float):
