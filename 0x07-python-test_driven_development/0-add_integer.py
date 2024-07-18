fd50b028aeb7639e3dc0c9d80df565f29f99e833
leonileo <kalebwendwessen@gmail.com>
2024-07-18 03:20:26 -0700

    add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,4 @@
-d#!/usr/bin/python3
+#!/usr/bin/python3
 # 0-add_integer.py
 """ Defines an integer addition function. """
 
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
-    if (not isinstance(a, int) and not isinstance(a, float))
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    elif(not isinstance(b, int) and not isinstance(b, float)):
+    elif type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
     else:
         return(int(a) + int(b))
