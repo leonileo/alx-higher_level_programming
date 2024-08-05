289b8b05618ab0363d77c7c08b71a8d22f3b5fab
leonileo <leolittleprogrammer@gmail.com>
2024-08-05 09:38:48 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
+""" This module is has the base class that will be the
+base class for the others in the coming project."""
+
+
 class Base:
+
+    """ This class has 1 private attribute
+    ```__nb_objects ``` that tracs the id of child classes.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -12,9 +22,13 @@ class Base:
 
     @property
     def nb_objects(self):
+        """ A getter function for the public attribute
+        __nb_objects"""
         return Base.__nb_objects
 
     @nb_objects.setter
     def nb_objects(self, val):
+        """ A setter function for the public attribute
+        __nb_objects"""
         Base.__nb_objects = Base.__nb_objects + val
         return Base.__nb_objects
