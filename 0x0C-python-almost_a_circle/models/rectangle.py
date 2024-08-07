e8f1272cd12a193ceffe5f72deab2cd1ad59a460
leonileo <leolittleprogrammer@gmail.com>
2024-08-07 13:32:15 -0700

    Update: updated the "update" method.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -43,19 +43,32 @@ class Rectangle(Base):
             self.__class__.__name__, self.id, self.x, self.y,
             self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ Assign arguments to the public attrivute """
-        for i in range(0, len(args)):
-            if i == 0:
-                self.id = args[0]
-            if i == 1:
-                self.width = args[1]
-            if i == 2:
-                self.height = args[2]
-            if i == 3:
-                self.x = args[3]
-            if i == 4:
-                self.y = args[4]
+        if args and len(args) != 0:
+            for i in range(0, len(args)):
+                if i == 0:
+                    self.id = args[0]
+                    if i == 1:
+                        self.width = args[1]
+                    if i == 2:
+                        self.height = args[2]
+                    if i == 3:
+                        self.x = args[3]
+                    if i == 4:
+                        self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     # getter and setter functions
 
