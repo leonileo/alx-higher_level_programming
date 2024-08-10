746177d37e1424d3a67967b7b1f8255876aa2af3
leonileo <leolittleprogrammer@gmail.com>
2024-08-10 10:01:36 -0700

    Update: addede getter and setter function

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,27 @@ class Square(Rectangle):
         self.size = size
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ Getter func for size """
+        return size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
+        self.width = value
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
+        self.height = value
+
     def __str__(self):
         return "[{}] ({}) {}/{} - {}".format(
             self.__class__.__name__, self.id, self.x, self.y, self.size)
