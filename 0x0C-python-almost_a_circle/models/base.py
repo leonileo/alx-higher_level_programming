f640b008a25ef97a1e5e6d12be2313c9e11743a6
leonileo <leolittleprogrammer@gmail.com>
2024-08-12 08:18:37 -0700

    Update: added to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,6 +36,7 @@ class Base:
         return Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation of the argument list_dictionaries """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
