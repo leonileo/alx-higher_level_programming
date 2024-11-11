84911616db1d1d2373acbfc15e43cc4d219ce7be
leonileo <leolittleprogrammer@gmail.com>
2024-11-11 07:49:30 -0800

    hot fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""This modlue has one function that adds two arguments by checking
-their types. The provided arguments must have a type of either
-integers or float, otherwise the function will raise a TypeError
-exception. If one of the arguments has a type of float it will
-cast them into integers and return the addition of the arguments."""
+
+"""
+This module provides one function called
+``add_integer()``.
+which returns the summation of the provided arguments.
+"""
 
 
 def add_integer(a, b=98):
-    """Checks the types of the two arguments,
-    then return their summation.
-    """
-    if (type(a) is not int and type(a) is not float):
+    """ This functions adds the provided arguments.
+    add_integer(1, 3)
+    4 """
+    if (not isinstance(a, (int, float)) or a == float('inf') or
+    a == float('-inf') or a != a):
         raise TypeError('a must be an integer')
-    elif (type(b) is not int and type(b) is not float):
+    if (not isinstance(b, (int, float)) or b == float('inf') or
+    b == float('-inf') or b != b):
         raise TypeError('b must be an integer')
-    else:
-        if type(a) is float:
-            a = int(a)
-        elif type(b) is float:
-            b = int(b)
-        return a + b
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
