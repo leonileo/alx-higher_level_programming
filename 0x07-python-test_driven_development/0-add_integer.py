0a377f4d6eab3cdb4ec002017f662d7ff264ab4a
leonileo <leolittleprogrammer@gmail.com>
2024-11-11 07:56:57 -0800

    indentation fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,10 +12,10 @@ def add_integer(a, b=98):
     add_integer(1, 3)
     4 """
     if (not isinstance(a, (int, float)) or a == float('inf') or
-    a == float('-inf') or a != a):
+       a == float('-inf') or a != a):
         raise TypeError('a must be an integer')
     if (not isinstance(b, (int, float)) or b == float('inf') or
-    b == float('-inf') or b != b):
+       b == float('-inf') or b != b):
         raise TypeError('b must be an integer')
     if isinstance(a, float):
         a = int(a)
