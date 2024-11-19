24f464f9bfddac4b9b052057ceb9b5cdcdc3bb54
leonileo <leolittleprogrammer@gmail.com>
2024-11-19 03:38:39 -0800

    Update: added validation for the setter methods and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,9 +9,32 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
+        # validaion for width value
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
         self.__width = width
+
+        # validation for height
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be > 0')
         self.__height = height
+
+        # validation for x
+        if not isinstance(x, int):
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
         self.__x = x
+
+        # validation for y
+        if not isinstance(y, int):
+            raise TypeError('y must be an integer')
+        if y < 0:
+            raise ValueError('y must be >= 0')
         self.__y = y
 
     @property
@@ -22,6 +45,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, val):
         """A public setter for the private instance attribute width"""
+        if not isinstance(val, int):
+            raise TypeError('width must be an integer')
+        if val <= 0:
+            raise ValueError('width must be > 0')
         self.__width = val
 
     @property
@@ -32,6 +59,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, val):
         """A public setter for the private instance attribute height"""
+        if not isinstance(val, int):
+            raise TypeError('height must be an integer')
+        if val <= 0:
+            raise ValueError('height must be > 0')
         self.__height = val
 
     @property
@@ -42,6 +73,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, val):
         """A public setter for the private instance attribute x"""
+        if not isinstance(val, int):
+            raise TypeError('x must be an integer')
+        if val < 0:
+            raise ValueError('x must be >= 0')
         self.__x = val
 
     @property
@@ -52,4 +87,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, val):
         """A public setter for the private instance attribute y"""
+        if not isinstance(val, int):
+            raise TypeError('y must be an integer')
+        if val < 0:
+            raise ValueError('y must be >= 0')
         self.__y = val
