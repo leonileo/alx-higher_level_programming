a124d9a48386c87f6ad98e53cb369bcb5f763fac
leonileo <leolittleprogrammer@gmail.com>
2024-11-19 01:45:00 -0800

    Models - Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Base class"""
+
+
+class Base:
+
+    __nb_objects = 0
+
+    # instantiation
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
