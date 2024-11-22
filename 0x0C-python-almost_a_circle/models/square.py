0c0640e3f072788ba4fb118e2a534920e705210a
leonileo <leolittleprogrammer@gmail.com>
2024-11-22 08:28:02 -0800

    Update: added public getter and setters

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,8 +7,17 @@ class Square(Rectangle):
     """Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
         self.size = size
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
