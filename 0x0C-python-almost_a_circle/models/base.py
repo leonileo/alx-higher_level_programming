20dff620f6b7e55ac376a98e43a0452d7d3ea523
leonileo <leolittleprogrammer@gmail.com>
2024-11-22 14:40:28 -0800

    Update: added the save_to_file() method.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,13 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) <= 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of 'list_objs' to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, "a", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            lists = [i.to_dictionary() for i in list_objs]
+            f.write(Base.to_json_string(lists))
