96bba5e69256ba531db2e24fa1bfcb026211b5e9
leonileo <leolittleprogrammer@gmail.com>
2024-11-22 14:11:07 -0800

    Update: added to_json_string() method.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class"""
+import json
 
 
 class Base:
@@ -14,3 +15,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the K=JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
