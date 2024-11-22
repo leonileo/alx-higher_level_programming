d60552ec09ddbaea01c9895efef54d2e9c868a7c
leonileo <leolittleprogrammer@gmail.com>
2024-11-22 08:45:01 -0800

    Update: added the update() function.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,3 +23,17 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.size
         )
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes """
+        if args:
+            self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        if not args:
+            for key, attr in kwargs.items():
+                setattr(self, key, attr)
