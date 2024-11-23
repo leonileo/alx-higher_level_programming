49df0f15d8d6588bc9b51f2b1db422fd9091c532
leonileo <leolittleprogrammer@gmail.com>
2024-11-22 16:18:14 -0800

    Update: added the from_json_string() method.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,11 @@ class Base:
             else:
                 lists = [i.to_dictionary() for i in list_objs]
                 f.write(Base.to_json_string(lists))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation 'json_string'."""
+        if json_string is None or len(json_string) < 0:
+            return "[]"
+        else:
+            return json.loads(json_string)
