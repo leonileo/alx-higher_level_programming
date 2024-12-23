84e358832bea051427d7dee45f0caeb382717c0d
leonileo <leolittleprogrammer@gmail.com>
2024-12-23 07:07:25 -0800

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Full creation
+CREATE TABLE IF NOT EXISTS second_table (
+       id INT,
+       name VARCHAR(256),
+       score INT
+);
+
+INSERT INTO second_table
+VALUES (1, "John", 10)
+VALUES (2, "Alex", 3)
+VALUES (3, "Bob", 14)
+VALUES (4, "George", 8)
+;
