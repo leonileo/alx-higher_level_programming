6a8367da281b749ca9ba96939d22e17cc456ea3c
leonileo <leolittleprogrammer@gmail.com>
2024-12-23 07:08:20 -0800

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,8 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 
 INSERT INTO second_table
-VALUES (1, "John", 10)
-VALUES (2, "Alex", 3)
-VALUES (3, "Bob", 14)
-VALUES (4, "George", 8)
-;
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
