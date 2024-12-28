7a7fc96e81f71b0906a5dee6f31d86b2192ff77b
leonileo <leolittleprogrammer@gmail.com>
2024-12-28 12:19:59 -0800

    Readme

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,5 +0,0 @@
--- Creates the table unique_id.
-CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id`   INT          DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256)
-);
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,9 +0,0 @@
--- Lists all cities of CA in the database hbtn_0d_usa.
--- Results are ordered by ascending cities.id.
-SELECT `id`, `name`
-  FROM `cities`
- WHERE `state_id` IN
-       (SELECT `id`
-	  FROM `states`
-	 WHERE `name` = "California")
- ORDER BY `id`;
